e4e18ad171a9a9d90be89f17d495318d9559f77d
Geoff Guidetti <geoffguidetti@gmail.com>
2018-10-28 23:36:09 -0700

    commits

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -9,10 +8,9 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+try {
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 // Add headers
 app.use(function (req, res, next) {
@@ -58,7 +56,8 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  console.log('is 404');
+  res.status(404).send('error');
 });
 
 // error handler
@@ -66,12 +65,12 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  console.error(err);
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render({ error: err});
 });
-
-app.listen(3005, () => console.log(`Example app listening on port ${3005}!`))
-
+} catch(e) {
+  console.error(e);
+}
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,15 @@
 var express = require('express');
+var connection = require('../config');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  const result = {
-    hello: true
-  }
-  res.status(200).send(result);
+router.get('/players', function(req, res, next) {
+  console.log('get players');
+  connection.promise().query('SELECT * from players').then(([results, fields]) => {
+    console.log('result', results);
+    res.status(200).send(results);
+  });
+
 });
 
 module.exports = router;
