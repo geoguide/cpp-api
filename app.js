889ed25b2a209083f3a7e0441bf1f48eca2df8c8
Geoff Guidetti <geoffguidetti@gmail.com>
2018-10-28 20:03:11 -0700

    preparing for autodeploy

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser'); //For parsing json put requests https://github.com/expressjs/body-parser
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -12,6 +13,39 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+// Add headers
+app.use(function (req, res, next) {
+	//TO DO: enable security functions, slim down access control
+
+	// Website to allow to connect
+	res.setHeader('Access-Control-Allow-Origin', '*');
+
+	// Request methods to allow
+	res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+
+	// Request headers to allow
+	res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, APIVersion, modio");
+
+	// Set to true if need the website to include cookies in the requests sent to the API (for future IE 7 integration)
+	res.setHeader('Access-Control-Allow-Credentials', true);
+
+	res.setHeader('Cache-Control', 'no-store, no-cache');
+
+  res.setHeader('Content-Type', 'application/json');
+
+	// Pass it on
+	next();
+});
+
+app.set('x-powered-by',false);
+app.set('json spaces',2);
+
+/*******************
+       Parsers
+*******************/
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -37,5 +71,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.listen(3005, () => console.log(`Example app listening on port ${3005}!`))
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const result = {
+    hello: true
+  }
+  res.status(200).send(result);
 });
 
 module.exports = router;
